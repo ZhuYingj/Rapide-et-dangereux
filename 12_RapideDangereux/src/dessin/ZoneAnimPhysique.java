2a08a6851c2e6eb19dc47c5d8c2179abe7abcd4c
Kevin Nguyen <darkeavin@outlook.com>
2023-02-22 09:41:27 -0500

    methode   de base zone anim physique

--- 12_RapideDangereux/src/dessin/ZoneAnimPhysique.java
+++ 12_RapideDangereux/src/dessin/ZoneAnimPhysique.java
@@ -7,10 +7,16 @@ import java.awt.RenderingHints;
 
 import javax.swing.JPanel;
 
-public class ZoneAnimPhysique extends JPanel {
+public class ZoneAnimPhysique extends JPanel implements Runnable {
 	
 	/** Nombre de pixels pas metre. */
 	private double pixelsParMetre;
+	/** Temps du deltaT par d�faut */
+	private double deltaT = 0.01;
+	/** Booleen de l'animation initialise a false */
+	private boolean enCoursDAnimation = false;
+	/** Temps du sleep de l'application */
+	private int tempsDuSleep = 10;
 
 	public ZoneAnimPhysique() {
 		setBackground(Color.gray);
@@ -24,6 +30,89 @@ public class ZoneAnimPhysique extends JPanel {
 		
 	}
 	
+	public void run() {
+
+		while (enCoursDAnimation) {
+
+			calculerUneIterationPhysique();
+
+
+			repaint();
+			try {
+				Thread.sleep(tempsDuSleep);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		} // fin while
+	}
+	
+	public void demarrer() {
+
+		if (!enCoursDAnimation) {
+			Thread proc = new Thread(this);
+			proc.start();
+			enCoursDAnimation = true;
+
+		}
+
+	}
+	
+	
+	/**
+	 * Change le temps pour le sleep du thread.
+	 * 
+	 * @param tempsDuSleep Nouveua temps a appliquer au sleep.
+	 */
+	// Kevin Nguyen
+	public void setTempsDuSleep(int tempsDuSleep) {
+		this.tempsDuSleep = tempsDuSleep;
+	}
+
+	/**
+	 * Retourne le temps de sleep actuel.
+	 * 
+	 * @return temps du sleep actuel.
+	 */
+	// Kevin Nguyen
+	public int getTempsDuSleep() {
+		return tempsDuSleep;
+	}
+
+	/**
+	 * Modifie le pas (intervalle) de la simulation.
+	 * 
+	 * @param deltaT le pas (intervalle) de la simulation, exprime en secondes.
+	 */
+	// Kevin Nguyen
+	public void setDeltaT(double deltaT) {
+		this.deltaT = deltaT;
+	}
+
+	/**
+	 * Retourne le pas intervalle) de la simulation.
+	 * 
+	 * @return le pas intervalle) de la simulation, exprime en secondes.
+	 */
+	// Kevin Nguyen
+	public double getDeltaT() {
+		return deltaT;
+	}
+	
+	public void avancerUnPas() {
+		arreter();
+		calculerUneIterationPhysique();
+		repaint();
+	}
+
+	private void arreter() {
+		enCoursDAnimation = false;
+		repaint();
+		
+	}
+
+	private void calculerUneIterationPhysique() {
+		
+	}
 	
 	
 }
