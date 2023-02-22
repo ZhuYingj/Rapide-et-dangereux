3d4eae16b4f8f9a4ae2d306cea9114ce7e1cd296
Tan Tommy Rin <tantommyrin@gmail.com>
2023-02-21 21:59:28 -0500

    Création de la fenêtre sans le mode scientifique

--- 12_RapideDangereux/src/fenetre/FenetreJeuSansScientifique.java
+++ 12_RapideDangereux/src/fenetre/FenetreJeuSansScientifique.java
@@ -4,6 +4,13 @@ import java.awt.Font;
 
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import dessin.ZoneAnimPhysique;
+import javax.swing.border.TitledBorder;
+import javax.swing.border.EtchedBorder;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.JProgressBar;
+import javax.swing.SwingConstants;
 
 public class FenetreJeuSansScientifique extends JPanel {
 
@@ -13,10 +20,28 @@ public class FenetreJeuSansScientifique extends JPanel {
 	public FenetreJeuSansScientifique() {
 		setLayout(null);
 		setBounds(100, 100, 1300, 700);
+		
+		ZoneAnimPhysique zoneAnimPhysique = new ZoneAnimPhysique();
+		zoneAnimPhysique.setBorder(new TitledBorder(new EtchedBorder(EtchedBorder.LOWERED, new Color(255, 255, 255), new Color(160, 160, 160)), "ZONE D'ANIMATION", TitledBorder.CENTER, TitledBorder.TOP, null, new Color(0, 0, 0)));
+		zoneAnimPhysique.setBounds(10, 33, 1280, 656);
+		add(zoneAnimPhysique);
+		zoneAnimPhysique.setLayout(null);
+		
+		JPanel panelObjet = new JPanel();
+		panelObjet.setBounds(936, 24, 323, 226);
+		zoneAnimPhysique.add(panelObjet);
+		panelObjet.setLayout(null);
+		panelObjet.setBorder(new LineBorder(new Color(0, 0, 0)));
+		
+		JProgressBar progressBarFroce = new JProgressBar();
+		progressBarFroce.setOrientation(SwingConstants.VERTICAL);
+		progressBarFroce.setBounds(279, 11, 34, 204);
+		panelObjet.add(progressBarFroce);
+		progressBarFroce.setStringPainted(true);
+		progressBarFroce.setFont(new Font("Tahoma", Font.BOLD, 12));
 		JLabel lblTitreModeScientifique = new JLabel("Jeu");
 		lblTitreModeScientifique.setFont(new Font("Tahoma", Font.BOLD, 18));
-		lblTitreModeScientifique.setBounds(this.getWidth() / 2 - lblTitreModeScientifique.getWidth(), 0, 223, 22);
+		lblTitreModeScientifique.setBounds(650, 0, 47, 22);
 		add(lblTitreModeScientifique);
 	}
-
 }
