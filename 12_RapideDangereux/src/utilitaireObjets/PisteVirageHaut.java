e2626e2b478c1d97e0518929f2fb9090d119e818
TanTommyRin <tantommyrin@gmail.com>
2023-03-01 15:35:52 -0500

    Merge branch 'master' of https://gitlab.com/alexiskp21/12_rapidedangereux.git

--- 12_RapideDangereux/src/utilitaireObjets/PisteVirageHaut.java
+++ 12_RapideDangereux/src/utilitaireObjets/PisteVirageHaut.java
@@ -7,35 +7,34 @@ import java.awt.Stroke;
 
 import interfaces.Dessinable;
 
-public class PisteVirageHaut implements Dessinable{
+public class PisteVirageHaut implements Dessinable {
 
-private static final int TAILLE_PISTE = 80;
-	
+	private static final int TAILLE_PISTE = 80;
+
 	private int x;
 	private int y;
 	private int pente;
-	
+
 	public PisteVirageHaut(int x, int y) {
 		this.x = x;
 		this.y = y;
-		this.pente = ()
-		
-	
+
 	}
-	
+
 	@Override
 	public void dessiner(Graphics2D g2d) {
 		g2d.setColor(Color.BLACK);
-		g2d.fillRect(x, y, TAILLE_PISTE,TAILLE_PISTE);
+		g2d.fillRect(x, y, TAILLE_PISTE, TAILLE_PISTE);
 		g2d.setColor(Color.RED);
 		Stroke stroke = new BasicStroke(3f);
 		g2d.setStroke(stroke);
-		g2d.drawLine(x, y, x, y + (TAILLE_PISTE/3));
-		g2d.drawLine(x, y + (TAILLE_PISTE/3), x + (TAILLE_PISTE/3), y + ((TAILLE_PISTE/3)*2));
-		g2d.drawLine(x + (TAILLE_PISTE/3),  y + ((TAILLE_PISTE/3)*2), x + ((TAILLE_PISTE/3)*2), y + TAILLE_PISTE );
-		g2d.drawLine(x + ((TAILLE_PISTE/3)*2), y + TAILLE_PISTE, x + TAILLE_PISTE, y + TAILLE_PISTE  );
-	//	g2d.fillRect(x+TAILLE_PISTE, y, -3-3);
-		
+		g2d.drawLine(x, y, x, y + (TAILLE_PISTE / 3));
+		g2d.drawLine(x, y + (TAILLE_PISTE / 3), x + (TAILLE_PISTE / 3), y + ((TAILLE_PISTE / 3) * 2));
+		g2d.drawLine(x + (TAILLE_PISTE / 3), y + ((TAILLE_PISTE / 3) * 2), x + ((TAILLE_PISTE / 3) * 2),
+				y + TAILLE_PISTE);
+		g2d.drawLine(x + ((TAILLE_PISTE / 3) * 2), y + TAILLE_PISTE, x + TAILLE_PISTE, y + TAILLE_PISTE);
+		// g2d.fillRect(x+TAILLE_PISTE, y, -3-3);
+
 	}
-	
+
 }
