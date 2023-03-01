28b5af96323b3526e3710d2c15893fdd6509e10a
e1960411 <e1960411@D3758-21.cmaisonneuve.qc.ca>
2023-03-01 16:55:15 -0500

    commit

---
+++ 12_RapideDangereux/src/dessin/ZoneApercupiste.java
@@ -0,0 +1,42 @@
+package dessin;
+
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Image;
+import java.io.IOException;
+import java.net.URL;
+
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+
+
+
+public class ZoneApercupiste extends JPanel {
+
+//	private Image img = null;
+//	
+//	public ZoneApercupiste() {
+//		
+//
+//	URL  pisteMexique =  getClass().getClassLoader().getResource("PisteMexique.png");
+//
+//	Image img=null;
+//    try {
+//img = ImageIO.read(pisteMexique);
+//    }
+//    catch (IOException e) {
+//System.out.println("Erreur pendant la lecture du fichier d'image");
+//    }
+//
+//	
+//	}	
+//	
+//	public void paintComponent(Graphics g) {
+//		super.paintComponent(g);
+//		Graphics2D g2d = (Graphics2D) g;
+//		g2d.drawImage(img, 0, 0, null);
+//	   }	
+
+	
+
+}
