cfceed44ce76f8d9916e987ca815a58f57cc4ef8
Kevin Nguyen <darkeavin@outlook.com>
2023-03-24 14:15:11 -0400

    tour complet italie

--- 12_RapideDangereux/src/pisteDeCourse/PisteItalie.java
+++ 12_RapideDangereux/src/pisteDeCourse/PisteItalie.java
@@ -18,15 +18,15 @@ import utilitaireObjets.PisteVirageGauche;
 import utilitaireObjets.PisteVirageHaut;
 import utilitaireObjets.Voiture;
 
-public class PisteItalie implements Dessinable{
+public class PisteItalie implements Dessinable {
 
 	/** Taille de la piste qui est toujours constante **/
 	private int taillePiste = 80;
 	/** Position en x de la piste au complet **/
 	private int x;
-	/** Position en y de la piste au complet  **/
+	/** Position en y de la piste au complet **/
 	private int y;
-	
+
 	private List<PisteDeDepart> depart = new ArrayList<PisteDeDepart>();
 
 	private List<PisteHorizontale> horizontale = new ArrayList<PisteHorizontale>();
@@ -35,159 +35,155 @@ public class PisteItalie implements Dessinable{
 	private List<PisteVirageGauche> gauche = new ArrayList<PisteVirageGauche>();
 	private List<PisteVirageDroit> droit = new ArrayList<PisteVirageDroit>();
 	private List<PisteVirageHaut> haut = new ArrayList<PisteVirageHaut>();
-	private Accelerateur vitesse = new Accelerateur(0,0);
+	private Accelerateur vitesse = new Accelerateur(0, 0);
 	private Rectangle2D rectangle;
 	private Area aireRectangle;
 	private double pixelsParMetre;
 	private int TAILLE_PISTE;;
-	
-	
+
 	public PisteItalie(int x, int y) {
 		this.x = x;
 		this.y = y;
 		creerLaGeometrie();
 	}
-	
+
 	public void creerLaGeometrie() {
 		bas.add(new PisteVirageBas(x, y));
 		this.x = x + taillePiste;
-		
-		
-		//piste horizontale depart :
+
+		// piste horizontale depart :
 		depart.add(new PisteDeDepart(x, y));
 		this.x = x + taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
 		this.x = x + taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
 		this.x = x + taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
-	
+
 		this.x = x + taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
-	
+
 		this.x = x + taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
-		
+
 		this.x = x + taillePiste;
-		
-		//Piste virage vers la gauche:
+
+		// Piste virage vers la gauche:
 		gauche.add(new PisteVirageGauche(x, y));
-		
+
 		this.y = y + taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
 		//
 		this.y = y + taillePiste;
-	
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
 		//
 		this.y = y + taillePiste;
-	
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
 		//
 		this.y = y + taillePiste;
-		
-		//piste vers le droit:
+
+		// piste vers le droit:
 		droit.add(new PisteVirageDroit(x, y));
 		//
-		this.x = x - taillePiste;	
-		
-		//Piste Virage Haut;
+		this.x = x - taillePiste;
+
+		// Piste Virage Haut;
 		haut.add(new PisteVirageHaut(x, y));
-	
+
 		this.y = y - taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
-		//	
+		//
 		this.y = y - taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
-		//	
+		//
 		this.y = y - taillePiste;
-		
-		//Piste virage vers la gauche:
+
+		// Piste virage vers la gauche:
 		gauche.add(new PisteVirageGauche(x, y));
 
 		this.x = x - taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
-	
+
 		this.x = x - taillePiste;
-		
-		//piste horizontale :
+
+		// piste horizontale :
 		horizontale.add(new PisteHorizontale(x, y));
-		
+
 		this.x = x - taillePiste;
-	
-		//piste virgae bas :
+
+		// piste virgae bas :
 		bas.add(new PisteVirageBas(x, y));
-		
+
 		this.y = y + taillePiste;
-		
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
 		//
 		this.y = y + taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
 		//
 		this.y = y + taillePiste;
-		
-		//piste vers le droit:
+
+		// piste vers le droit:
 		droit.add(new PisteVirageDroit(x, y));
 		//
 		this.x = x - taillePiste;
-		
-		//piste horizontale :
-		horizontale.add(new PisteHorizontale(x, y));
-	
-		this.x = x - taillePiste;
-		
-		//piste horizontale :
-		horizontale.add(new PisteHorizontale(x, y));
-	
-		this.x = x - taillePiste;
-		
 
-		//Piste Virage Haut;
+		// piste horizontale :
+		horizontale.add(new PisteHorizontale(x, y));
+
+		this.x = x - taillePiste;
+
+		// piste horizontale :
+		horizontale.add(new PisteHorizontale(x, y));
+
+		this.x = x - taillePiste;
+
+		// Piste Virage Haut;
 		haut.add(new PisteVirageHaut(x, y));
-	
+
 		this.y = y - taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
-		//	
+		//
 		this.y = y - taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
-		//	
+		//
 		this.y = y - taillePiste;
-		
-		//piste vertical:
+
+		// piste vertical:
 		verticale.add(new PisteVerticale(x, y));
-		//	
+		//
 		this.y = y - taillePiste;
 	}
-	
+
 	@Override
 	public void dessiner(Graphics2D g2d) {
 		Graphics2D gCopie = (Graphics2D) g2d.create();
@@ -223,9 +219,8 @@ public class PisteItalie implements Dessinable{
 
 		depart.get(0).dessiner(gCopie);
 
-		
-		
 	}
+
 	public double getPixelsParMetre() {
 		return pixelsParMetre;
 	}
@@ -241,7 +236,7 @@ public class PisteItalie implements Dessinable{
 	public void setDepart(List<PisteDeDepart> depart) {
 		this.depart = depart;
 	}
-	
+
 	public void enCollisionAvec(Voiture voiture) {
 
 		for (int i = 0; i < horizontale.size(); i++) {
@@ -249,9 +244,9 @@ public class PisteItalie implements Dessinable{
 			horizontale.get(i).traverserPiste(voiture);
 			if (horizontale.get(i).isCollision() == true) {
 				horizontale.get(i).setColor(Color.blue);
-				boolean  collision = true;
-				
-			}else {
+				boolean collision = true;
+
+			} else {
 				horizontale.get(i).setColor(Color.black);
 			}
 		}
@@ -278,6 +273,116 @@ public class PisteItalie implements Dessinable{
 
 		depart.get(0).enCollisionAvec(voiture);
 //		traverserPiste(voiture);
+		tourComplet(voiture);
+	}
+
+	public void tourComplet(Voiture voiture) {
+
+		for (int i = 0; i < horizontale.size(); i++) {
+			horizontale.get(i).isCollision();
+		}
+		int count = 0;
+		for (int i = 0; i < horizontale.size(); i++) {
+			if (horizontale.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+
+		for (int i = 0; i < bas.size(); i++) {
+			bas.get(i).isCollision();
+		}
+
+		for (int i = 0; i < bas.size(); i++) {
+			if (bas.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+
+		for (int i = 0; i < haut.size(); i++) {
+			haut.get(i).isCollision();
+		}
+
+		for (int i = 0; i < haut.size(); i++) {
+			if (haut.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+		for (int i = 0; i < gauche.size(); i++) {
+			gauche.get(i).isCollision();
+		}
+
+		for (int i = 0; i < gauche.size(); i++) {
+			if (gauche.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+		for (int i = 0; i < droit.size(); i++) {
+			droit.get(i).isCollision();
+		}
+
+		for (int i = 0; i < droit.size(); i++) {
+			if (droit.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+		for (int i = 0; i < depart.size(); i++) {
+			depart.get(i).isCollision();
+		}
+
+		for (int i = 0; i < depart.size(); i++) {
+			if (depart.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+		for (int i = 0; i < verticale.size(); i++) {
+			verticale.get(i).isCollision();
+		}
+
+		for (int i = 0; i < verticale.size(); i++) {
+			if (verticale.get(i).isCollision() == true) {
+				count++;
+			}
+		}
+
+		if (count == horizontale.size() + verticale.size() + bas.size() + haut.size() + gauche.size() + droit.size()
+				+ depart.size()) {
+
+			if (depart.get(0).resetTout(voiture)) {
+				resetTour();
+
+			}
+		}
+	}
+
+	public void resetTour() {
+
+		for (int i = 0; i < droit.size(); i++) {
+			droit.get(i).setCollision(false);
+
+		}
+		for (int i = 0; i < depart.size(); i++) {
+			depart.get(i).setCollision(false);
+		}
+
+		for (int i = 0; i < horizontale.size(); i++) {
+			horizontale.get(i).setCollision(false);
+
+		}
+		for (int i = 0; i < verticale.size(); i++) {
+			verticale.get(i).setCollision(false);
+		}
+
+		for (int i = 0; i < gauche.size(); i++) {
+			gauche.get(i).setCollision(false);
+		}
+
+		for (int i = 0; i < haut.size(); i++) {
+			haut.get(i).setCollision(false);
+		}
+
+		for (int i = 0; i < gauche.size(); i++) {
+			bas.get(i).setCollision(false);
+		}
 
 	}
 }
