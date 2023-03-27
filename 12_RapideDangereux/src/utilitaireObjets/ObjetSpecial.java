f96439cb7f1c428d0e7a9c43efef61ac7e5b0cf2
Alexis Pineda <minialpineda@live.ca>
2023-03-26 20:20:06 -0400

    depot

--- 12_RapideDangereux/src/utilitaireObjets/ObjetSpecial.java
+++ 12_RapideDangereux/src/utilitaireObjets/ObjetSpecial.java
@@ -1,12 +1,8 @@
 package utilitaireObjets;
 
-import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.Shape;
 import java.awt.geom.AffineTransform;
-import java.awt.geom.Ellipse2D;
 
-import dessin.ZoneAnimPhysique;
 import geometrie.Vecteur2D;
 import interfaces.Dessinable;
 import interfaces.TypeObjetSpecial;
@@ -26,7 +22,6 @@ public class ObjetSpecial implements Dessinable {
 	private double pixelParMetre = 1;
 	private double tempsTemporaire;
 	private boolean fonctionActive = false;
-
 
 	/**
 	 * Méthode permettant de créer un objet spécial à l'aide de paramètre
@@ -54,8 +49,8 @@ public class ObjetSpecial implements Dessinable {
 
 		}
 		if (type == TypeObjetSpecial.BOULEDENEIGE) {
-			BouleDeNeige bouleDeNeige = new BouleDeNeige(this.positionObjet, this.diametreObjet, type);
-			
+			BouleDeNeige bouleDeNeige = new BouleDeNeige(new Vecteur2D(10, 10), 15.0, TypeObjetSpecial.BOULEDENEIGE);
+			bouleDeNeige.dessiner(g2d);
 		}
 
 	}
@@ -85,9 +80,9 @@ public class ObjetSpecial implements Dessinable {
 			fonctionChampignon(voiture, tempsTotalEcoule);
 
 		} else if (type == TypeObjetSpecial.BOULEDENEIGE) {
-			
+
 			fonctionBouleDeNeige(voiture, tempsTotalEcoule);
-			
+
 		} else if (type == TypeObjetSpecial.COLLE) {
 
 		} else {
@@ -115,14 +110,14 @@ public class ObjetSpecial implements Dessinable {
 		}
 
 	}
-	
+
 	/**
 	 * 
-	 * @param voiture		la valeur de la voiture qui va être affecté
-	 * @param tempsFinal	le temps total finaux qui va être écoulé
-	 * @return				la valeur du fonctionnement de la boule de neige causer par le temps
+	 * @param voiture    la valeur de la voiture qui va être affecté
+	 * @param tempsFinal le temps total finaux qui va être écoulé
+	 * @return la valeur du fonctionnement de la boule de neige causer par le temps
 	 */
-	//Alexis Pineda-Alvarado
+	// Alexis Pineda-Alvarado
 	public boolean fonctionBouleDeNeige(Voiture voiture, double tempsFinal) {
 
 		if ((tempsTemporaire + 1 > tempsFinal)) {
@@ -132,7 +127,7 @@ public class ObjetSpecial implements Dessinable {
 			voiture.setSommeDesForces(voitureSlow);
 			System.out.println(voitureSlow);
 			return true;
-		} else {
+		}else {
 			System.out.println("NORMAL SPEED!!!");
 			Vecteur2D voitureNormal = new Vecteur2D();
 			voitureNormal = MoteurPhysique.calculerForceFrottement(0.45, voiture.getMasseEnKg(), voiture.getAngle());
