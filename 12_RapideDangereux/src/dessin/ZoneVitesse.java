185c258adb35e1c0c492586dfd66ee3d65200068
Ludovic Julien <ludovicjulien2002@outlook.com>
2023-04-14 13:10:44 -0400

    commit

--- 12_RapideDangereux/src/dessin/ZoneVitesse.java
+++ 12_RapideDangereux/src/dessin/ZoneVitesse.java
@@ -4,11 +4,14 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.RenderingHints;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 import javax.swing.JPanel;
+import javax.swing.Timer;
 
 import utilitaireObjets.Voiture;
 
